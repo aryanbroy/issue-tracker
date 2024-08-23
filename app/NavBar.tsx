49db87762926dcb4b377268975bc60a9f07af487
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-08-23 23:08:56 +0530

    styling the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,21 +1,35 @@
+'use client';
+
+import classNames from 'classnames';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { MdBugReport } from 'react-icons/md';
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' },
   ];
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href={'/'}>
         <MdBugReport size={24} />
       </Link>
       <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li>
-            <Link href={link.href} className="text-zinc-500 hover:text-zinc-800 transition-colors">
+        {links.map((link, i) => (
+          <li key={i}>
+            <Link
+              href={link.href}
+              className={classNames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true,
+              })}
+            >
               {link.label}
             </Link>
           </li>
---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Issue = () => {
+  return <div>Issues</div>;
+};
+
+export default Issue;
