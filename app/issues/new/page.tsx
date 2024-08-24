410ef71cf4959cb6ef44bb368a7a3ecfb07649b2
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-08-24 14:06:47 +0530

    implement react hook form for form management

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,28 @@
 'use client';
 
-import { Button, TextArea, TextField } from '@radix-ui/themes';
+import { Button, TextField } from '@radix-ui/themes';
+import { Controller, useForm } from 'react-hook-form';
+import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
 import React from 'react';
 
+interface IssuesForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm();
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title" size="2" />
-      <TextArea placeholder="Description" />
+    <form onSubmit={handleSubmit((data) => console.log(data))} className="max-w-xl space-y-3">
+      <TextField.Root placeholder="Title" size="2" {...register('title')} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
